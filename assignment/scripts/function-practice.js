a691f25894dc72c88a6c6b00731bef8b4976f9ef
Aden Ali <abdinasir55@gmail.com>
2024-10-16 17:47:53 -0500

    FINAL DRAFT

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,9 +14,6 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName(name) {
-  return;
-}
 
 function helloName(name) {
   return (`Hello, ${name}!`);
